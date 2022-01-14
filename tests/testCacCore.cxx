3c2d0debf1a1b4b5bd2560081fb0ee47f03f4c77
Chenhui Huang <chuang@tenstorrent.com>
2022-01-14 11:59:16 -0600

    Just a test for cosim

--- tests/testCacCore.cxx
+++ tests/testCacCore.cxx
@@ -31,11 +31,11 @@ TEST(TestCAC, SingleHart) {
     cac.updateRefRegister(tid0, CAC_STATE_PC_ID, PCValue1);
     cac.updateRefRegister(tid0, CAC_STATE_RegX0_ID, RegXXValue1);
     cac.updateRefRegister(tid0, CAC_STATE_RegX1_ID, RegXXValue0);
-    cac.updateRefRegister(tid0, CAC_STATE_RegX11_ID, RegXXValue3);
+    cac.updateRefRegister(tid0, CAC_STATE_RegX2_ID, RegXXValue3);
     //From DUT
     cac.updateRegister(tid0, CAC_STATE_PC_ID, PCValue1);
     cac.updateRegister(tid0, CAC_STATE_RegX1_ID, RegXXValue0);
-    cac.updateRegister(tid0, CAC_STATE_RegX11_ID, RegXXValue3);
+    cac.updateRegister(tid0, CAC_STATE_RegX2_ID, RegXXValue3);
     cac.updateRegister(tid0, CAC_STATE_RegX0_ID, RegXXValue1);
     //Single Step
     cac.step(tid0);
