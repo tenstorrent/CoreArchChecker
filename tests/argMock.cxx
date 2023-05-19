7b88cac2214b4ca166e53fb8a1c8014e955d3abc
Matt Boisvert <mboisvert@tenstorrent.com>
2023-05-19 16:10:54 -0500

    feat: Refactor tests and add CI to run them

---
+++ tests/argMock.cxx
@@ -0,0 +1,11 @@
+#include "cvm/plusargs.hpp"
+#include "vpi_user.h"
+
+DEFINE_bool(cosim_tracer, true, "Enable bridge trace prints");
+
+// Needed for plusargs
+extern "C" {
+    PLI_INT32 vpi_get_vlog_info (p_vpi_vlog_info vlog_info_p) {
+        return 0;
+    }
+}
